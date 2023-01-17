0676b43733e486dfeaeb25cbc236a4a592bedff6
alimuhamad1 <96423134+alimuhamad1@users.noreply.github.com>
2023-01-18 00:38:45 +0100

    Add files via upload

---
+++ demo/src/test/java/com/alimuhamad/webservice/demo/WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alimuhamad.webservice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
